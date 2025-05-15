e128100032096579ab92bade32400cf2495b6ff1
Carl Wico <139624156+WCARL12@users.noreply.github.com.>
2025-05-15 19:41:25 -0230

    implemented function to get the computer choice

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const rockButton = document.querySelector(".rockBtn")
 const paperButton = document.querySelector(".paperBtn")
 const scissorsButton = document.querySelector(".scissorsBtn")
 
+let computerChoice = "";
 
 rockButton.addEventListener("click", () => {
     
@@ -9,9 +10,23 @@ rockButton.addEventListener("click", () => {
 })
 
 function getComputerChoice(){
+
+    // Get a random number from 1-3 
     let randomNumber = Math.floor(Math.random() * 3 + 1)
-    console.log(randomNumber);
+    // console.log(randomNumber);
+
+    if (randomNumber === 1){
+        return "rock"
+    }
+
+    else if (randomNumber === 2){
+        return "paper"
+    }
+
+    return "scissors"
     
 }
 
-getComputerChoice()
+// computerChoice = getComputerChoice()
+
+
