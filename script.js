a487306f43e10d0a9412a2113cfe22863c0a6363
Your Name <139624156+WCARL12@users.noreply.github.com.>
2025-05-16 10:19:35 -0230

    Added starting screen and in game screen. Planning to add CSS next for starting screen

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-// const rockButton = document.querySelector(".rockBtn")
-// const paperButton = document.querySelector(".paperBtn")
-// const scissorsButton = document.querySelector(".scissorsBtn")
+// Get the starting button tag
+const startButton = document.querySelector(".startBtn")
 
-const buttons = document.querySelectorAll("button")
+// Get the starting screen div tag
+const startingScreen = document.querySelector(".start-screen")
 
+
+// Get the all rock-paper-scissor buttons
+const choiceButtons = document.querySelectorAll(".player-choices")
+
+// Tracks the score of player and computer
 let score = {
     playerScore: 0,
     computerScore : 0
 }
 
-
+// Variables to store player and computer choices
 let playerChoice = "";
 let computerChoice = "";
 
-function startGame(){
-    buttons.forEach(button => {
+
+
+function startGame(){    
+
+    choiceButtons.forEach(button => {
+        button.style.display = "inline";
+
         button.addEventListener("click", () => {
-            
             
             playerChoice = button.textContent;
             computerChoice = getComputerChoice()
@@ -33,7 +42,6 @@ function getComputerChoice(){
 
     // Get a random number from 1-3 
     let randomNumber = Math.floor(Math.random() * 3 + 1)
-    // console.log(randomNumber);
 
     if (randomNumber === 1){
         return "rock"
@@ -74,4 +82,9 @@ function displayScores(){
     console.log(`Player Score: ${score.playerScore}\nComputer Score: ${score.computerScore}`)
 }
 
-startGame()
+// startGame()
+
+startButton.addEventListener("click", () => {
+    startingScreen.style.display = "none";
+    startGame()
+})
